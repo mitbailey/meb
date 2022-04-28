978989187c01abb3da8a6cef92be2935134c6650
Mit Bailey <mitbailey99@gmail.com>
2022-04-28 12:20:32 -0400

    Added LGPL licensing, removed Makefile examples.

--- src/print_test.c
+++ src/print_test.c
@@ -2,10 +2,23 @@
  * @file main.c
  * @author Mit Bailey (mitbailey99@gmail.com)
  * @brief 
- * @version v1.0
+ * @version See Git tags for version information.
  * @date 2021.11.02
  * 
  * @copyright Copyright (c) 2021
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
 
